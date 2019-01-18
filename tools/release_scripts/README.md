9c510ec7bc55cfdb753f50cf74ff94e4993c29a6
Nicholas Frechette <zeno490@gmail.com>
2019-01-17 22:00:07 -0500

    Adding a python script to automate release testing.

---
+++ tools/release_scripts/README.md
@@ -0,0 +1,7 @@
+# Release scripts
+
+Validating a release requires a lot of work and to that end, some scripts were written to automate the process as much as possible.
+
+## test_everything.py
+
+This script runs the unit tests on every platform except iOS and Android. It will run every permutation possible.
---
+++ tools/release_scripts/test_everything.py
@@ -0,0 +1,54 @@
+import os
+import platform
+import subprocess
+import sys
+
+def get_platform_compilers():
+	if platform.system() == 'Windows':
+		return [ 'vs2015', 'vs2017' ]
+	elif platform.system() == 'Linux':
+		return [ 'gcc5', 'gcc6', 'gcc7', 'gcc8', 'clang4', 'clang5', 'clang6' ]
+	elif platform.system() == 'Darwin':
+		return [ 'osx' ]
+	else:
+		print('Unknown platform!')
+		sys.exit(1)
+
+def get_python_exe_name():
+	if platform.system() == 'Windows':
+		return 'python'
+	else:
+		return 'python3'
+
+if __name__ == "__main__":
+	os.environ['PYTHONIOENCODING'] = 'utf_8'
+
+	configs = [ 'debug', 'release' ]
+	archs = [ 'x86', 'x64' ]
+	compilers = get_platform_compilers()
+	simd_opts = [ '', '-avx', '-nosimd' ]
+	python_exe = get_python_exe_name()
+
+	cmd_args = []
+	for config in configs:
+		for arch in archs:
+			for compiler in compilers:
+				for simd in simd_opts:
+					args = [python_exe, 'make.py', '-compiler', compiler, '-cpu', arch, '-config', config, simd, '-build', '-unit_test', '-clean']
+					cmd_args.append([x for x in args if x])
+
+	root_dir = os.path.join(os.getcwd(), '../..')
+	os.chdir(root_dir)
+
+	for args in cmd_args:
+		cmd = " ".join(args)
+		print('Running command: "{}" ...'.format(cmd))
+		try:
+			subprocess.check_output(args)
+		except subprocess.CalledProcessError as e:
+			print('Failed command: {}'.format(cmd))
+			print(e.output.decode(sys.stdout.encoding))
+			sys.exit(1)
+
+	print('Done!')
+	sys.exit(0)
