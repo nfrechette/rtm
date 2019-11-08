1929a2fc511ac9584fa2a1bb4ef18db980332bce
Nicholas Frechette <zeno490@gmail.com>
2019-11-07 22:57:41 -0500

    fix: sonarcloud fixes

--- tests/sources/test_vector4f_mix_w.cpp
+++ tests/sources/test_vector4f_mix_w.cpp
@@ -27,5 +27,5 @@
 
 TEST_CASE("vector4f vector_mix<w * * *>", "[math][vector4]")
 {
-	test_vector_mix_impl<vector4f, float, mix4::w>(1.0E-6f);
+	test_vector_mix_impl<vector4f, float, mix4::w>(1.0E-6F);
 }
--- tests/sources/test_vector4f_mix_y.cpp
+++ tests/sources/test_vector4f_mix_y.cpp
@@ -27,5 +27,5 @@
 
 TEST_CASE("vector4f vector_mix<y * * *>", "[math][vector4]")
 {
-	test_vector_mix_impl<vector4f, float, mix4::y>(1.0E-6f);
+	test_vector_mix_impl<vector4f, float, mix4::y>(1.0E-6F);
 }
