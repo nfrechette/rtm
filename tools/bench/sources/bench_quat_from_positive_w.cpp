f203c6e627ce5e5176569ec6647f3d2a5470458f
Nicholas Frechette <zeno490@gmail.com>
2019-11-18 20:26:40 -0500

    feat(bench): add SSE2 variants

--- tools/bench/sources/bench_quat_from_positive_w.cpp
+++ tools/bench/sources/bench_quat_from_positive_w.cpp
@@ -66,6 +66,43 @@ inline quatf RTM_SIMD_CALL quat_from_positive_w_sse4_and(vector4f_arg0 input) RT
 }
 #endif
 
+#if defined(RTM_SSE2_INTRINSICS)
+inline quatf RTM_SIMD_CALL quat_from_positive_w_sse2_and(vector4f_arg0 input) RTM_NO_EXCEPT
+{
+#if defined(_MSC_VER)
+	constexpr __m128i masks = { 0xFFU, 0xFFU, 0xFFU, 0x7FU, 0xFFU, 0xFFU, 0xFFU, 0x7FU, 0xFFU, 0xFFU, 0xFFU, 0x7FU, 0xFFU, 0xFFU, 0xFFU, 0x7FU };
+#else
+	constexpr __m128i masks = { 0x7FFFFFFF7FFFFFFFULL, 0x7FFFFFFF7FFFFFFFULL };
+#endif
+
+	__m128 x2y2z2 = _mm_mul_ps(input, input);
+	__m128 one = _mm_set_ss(1.0F);
+	__m128 w_squared = _mm_sub_ss(_mm_sub_ss(_mm_sub_ss(one, x2y2z2), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(1, 1, 1, 1))), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(2, 2, 2, 2)));
+	w_squared = _mm_and_ps(w_squared, _mm_castsi128_ps(masks));
+	__m128 w = _mm_sqrt_ss(w_squared);
+	__m128 input_wyzx = _mm_shuffle_ps(input, input, _MM_SHUFFLE(0, 2, 1, 3));
+	__m128 result_wyzx = _mm_move_ss(input_wyzx, w);
+	return _mm_shuffle_ps(result_wyzx, result_wyzx, _MM_SHUFFLE(0, 2, 1, 3));
+}
+
+inline quatf RTM_SIMD_CALL quat_from_positive_w_sse2_and2(vector4f_arg0 input) RTM_NO_EXCEPT
+{
+#if defined(_MSC_VER)
+	constexpr __m128i masks = { 0xFFU, 0xFFU, 0xFFU, 0x7FU, 0xFFU, 0xFFU, 0xFFU, 0x7FU, 0xFFU, 0xFFU, 0xFFU, 0x7FU, 0xFFU, 0xFFU, 0xFFU, 0x7FU };
+#else
+	constexpr __m128i masks = { 0x7FFFFFFF7FFFFFFFULL, 0x7FFFFFFF7FFFFFFFULL };
+#endif
+
+	__m128 x2y2z2 = _mm_mul_ps(input, input);
+	__m128 one = _mm_set_ss(1.0F);
+	__m128 w_squared = _mm_sub_ss(_mm_sub_ss(_mm_sub_ss(one, x2y2z2), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(1, 1, 1, 1))), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(2, 2, 2, 2)));
+	w_squared = _mm_and_ps(w_squared, _mm_castsi128_ps(masks));
+	__m128 w = _mm_sqrt_ss(w_squared);
+	__m128 zzww = _mm_shuffle_ps(input, w, _MM_SHUFFLE(0, 0, 2, 2));
+	return _mm_shuffle_ps(input, zzww, _MM_SHUFFLE(2, 0, 1, 0));
+}
+#endif
+
 #if defined(RTM_NEON_INTRINSICS)
 inline quatf RTM_SIMD_CALL quat_from_positive_w_neon(vector4f_arg0 input) RTM_NO_EXCEPT
 {
@@ -106,6 +143,28 @@ static void bm_quat_from_positive_w_sse4_and(benchmark::State& state)
 }
 
 BENCHMARK(bm_quat_from_positive_w_sse4_and);
+#endif
+
+#if defined(RTM_SSE2_INTRINSICS)
+static void bm_quat_from_positive_w_sse2_and(benchmark::State& state)
+{
+	quatf q0 = quat_identity();
+
+	for (auto _ : state)
+		benchmark::DoNotOptimize(q0 = quat_from_positive_w_sse2_and(q0));
+}
+
+BENCHMARK(bm_quat_from_positive_w_sse2_and);
+
+static void bm_quat_from_positive_w_sse2_and2(benchmark::State& state)
+{
+	quatf q0 = quat_identity();
+
+	for (auto _ : state)
+		benchmark::DoNotOptimize(q0 = quat_from_positive_w_sse2_and2(q0));
+}
+
+BENCHMARK(bm_quat_from_positive_w_sse2_and2);
 #endif
 
 #if defined(RTM_NEON_INTRINSICS)
