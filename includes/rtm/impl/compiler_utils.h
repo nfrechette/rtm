5e74514522308acba64a3a260ecfcaafcf048870
Nicholas Frechette <zeno490@gmail.com>
2020-06-12 18:26:07 -0400

    fix: move compiler macros so we can re-use them in the file

--- includes/rtm/impl/compiler_utils.h
+++ includes/rtm/impl/compiler_utils.h
@@ -25,46 +25,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////
-// Because this library is made entirely of headers, we have no control over the
-// compilation flags used. However, in some cases, certain options must be forced.
-// To do this, every header is wrapped in two macros to push and pop the necessary
-// pragmas.
-//////////////////////////////////////////////////////////////////////////
-#if defined(_MSC_VER) && !defined(__clang__)
-	#define RTM_IMPL_FILE_PRAGMA_PUSH \
-		/* Disable fast math, it can hurt precision for little to no performance gain due to the heavy usage of intrinsics. */ \
-		__pragma(float_control(precise, on, push))
-
-	#define RTM_IMPL_FILE_PRAGMA_POP \
-		__pragma(float_control(pop))
-#else
-	#define RTM_IMPL_FILE_PRAGMA_PUSH
-	#define RTM_IMPL_FILE_PRAGMA_POP
-#endif
-
-//////////////////////////////////////////////////////////////////////////
-// Force inline macros for when it is necessary.
-//////////////////////////////////////////////////////////////////////////
-#if defined(_MSC_VER) && !defined(__clang__)
-	#define RTM_FORCE_INLINE __forceinline
-#elif defined(__GNUG__) || defined(__clang__)
-	#define RTM_FORCE_INLINE __attribute__((always_inline)) inline
-#else
-	#define RTM_FORCE_INLINE inline
-#endif
-
-//////////////////////////////////////////////////////////////////////////
-// Force no-inline macros for when it is necessary.
-//////////////////////////////////////////////////////////////////////////
-#if defined(_MSC_VER) && !defined(__clang__)
-	#define RTM_FORCE_NOINLINE __declspec(noinline)
-#elif defined(__GNUG__) || defined(__clang__)
-	#define RTM_FORCE_NOINLINE __attribute__((noinline))
-#else
-	#define RTM_FORCE_NOINLINE
-#endif
-
-//////////////////////////////////////////////////////////////////////////
 // Macro to identity GCC
 //////////////////////////////////////////////////////////////////////////
 #if defined(__GNUG__) && !defined(__clang__)
@@ -83,4 +43,44 @@
 //////////////////////////////////////////////////////////////////////////
 #if defined(_MSC_VER) && !defined(__clang__)
 	#define RTM_COMPILER_MSVC
+#endif
+
+//////////////////////////////////////////////////////////////////////////
+// Because this library is made entirely of headers, we have no control over the
+// compilation flags used. However, in some cases, certain options must be forced.
+// To do this, every header is wrapped in two macros to push and pop the necessary
+// pragmas.
+//////////////////////////////////////////////////////////////////////////
+#if defined(RTM_COMPILER_MSVC)
+	#define RTM_IMPL_FILE_PRAGMA_PUSH \
+		/* Disable fast math, it can hurt precision for little to no performance gain due to the heavy usage of intrinsics. */ \
+		__pragma(float_control(precise, on, push))
+
+	#define RTM_IMPL_FILE_PRAGMA_POP \
+		__pragma(float_control(pop))
+#else
+	#define RTM_IMPL_FILE_PRAGMA_PUSH
+	#define RTM_IMPL_FILE_PRAGMA_POP
+#endif
+
+//////////////////////////////////////////////////////////////////////////
+// Force inline macros for when it is necessary.
+//////////////////////////////////////////////////////////////////////////
+#if defined(RTM_COMPILER_MSVC)
+	#define RTM_FORCE_INLINE __forceinline
+#elif defined(RTM_COMPILER_GCC) || defined(RTM_COMPILER_CLANG)
+	#define RTM_FORCE_INLINE __attribute__((always_inline)) inline
+#else
+	#define RTM_FORCE_INLINE inline
+#endif
+
+//////////////////////////////////////////////////////////////////////////
+// Force no-inline macros for when it is necessary.
+//////////////////////////////////////////////////////////////////////////
+#if defined(RTM_COMPILER_MSVC)
+	#define RTM_FORCE_NOINLINE __declspec(noinline)
+#elif defined(RTM_COMPILER_GCC) || defined(RTM_COMPILER_CLANG)
+	#define RTM_FORCE_NOINLINE __attribute__((noinline))
+#else
+	#define RTM_FORCE_NOINLINE
 #endif
