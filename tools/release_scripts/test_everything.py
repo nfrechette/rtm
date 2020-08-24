60e61b4fa37b01817e931164a30b21150b260354
Nicholas Frechette <zeno490@gmail.com>
2020-08-23 22:08:21 -0400

    refactor(tests): add switch to control inclusion of vector_mix unit tests
    The vector_mix unit tests take a long time to compile. We strip them out
    by default since that code is unlikely to change. CI won't be testing them
    but we'll test them every release.

--- tools/release_scripts/test_everything.py
+++ tools/release_scripts/test_everything.py
@@ -73,33 +73,33 @@ if __name__ == "__main__":
 						continue	# Hack to avoid compiler issue
 					if compiler == 'clang8' and simd == '-nosimd':
 						continue	# Hack to avoid compiler issue
-					args = [python_exe, 'make.py', '-compiler', compiler, '-cpu', arch, '-config', config, simd, '-build', '-unit_test', '-clean']
+					args = [python_exe, 'make.py', '-compiler', compiler, '-cpu', arch, '-config', config, simd, '-build', '-unit_test', '-vector_mix_test', '-clean']
 					cmd_args.append([x for x in args if x])
 
 	if platform.system() == 'Windows':
 		for config in configs:
 			# Windows ARM
-			args = [python_exe, 'make.py', '-compiler', 'vs2017', '-cpu', 'arm64', '-config', config, '-build', '-clean']
+			args = [python_exe, 'make.py', '-compiler', 'vs2017', '-cpu', 'arm64', '-config', config, '-build', '-vector_mix_test', '-clean']
 			cmd_args.append([x for x in args if x])
-			args = [python_exe, 'make.py', '-compiler', 'vs2017', '-cpu', 'arm64', '-config', config, '-build', '-clean', '-nosimd']
+			args = [python_exe, 'make.py', '-compiler', 'vs2017', '-cpu', 'arm64', '-config', config, '-build', '-vector_mix_test', '-clean', '-nosimd']
 			cmd_args.append([x for x in args if x])
-			args = [python_exe, 'make.py', '-compiler', 'vs2019', '-cpu', 'arm64', '-config', config, '-build', '-clean']
+			args = [python_exe, 'make.py', '-compiler', 'vs2019', '-cpu', 'arm64', '-config', config, '-build', '-vector_mix_test', '-clean']
 			cmd_args.append([x for x in args if x])
-			args = [python_exe, 'make.py', '-compiler', 'vs2019', '-cpu', 'arm64', '-config', config, '-build', '-clean', '-nosimd']
+			args = [python_exe, 'make.py', '-compiler', 'vs2019', '-cpu', 'arm64', '-config', config, '-build', '-vector_mix_test', '-clean', '-nosimd']
 			cmd_args.append([x for x in args if x])
 	elif platform.system() == 'Darwin':
 		for config in configs:
 			# iOS
-			args = [python_exe, 'make.py', '-compiler', 'ios', '-config', config, '-build', '-clean']
+			args = [python_exe, 'make.py', '-compiler', 'ios', '-config', config, '-build', '-vector_mix_test', '-clean']
 			cmd_args.append([x for x in args if x])
-			args = [python_exe, 'make.py', '-compiler', 'ios', '-config', config, '-build', '-clean', '-nosimd']
+			args = [python_exe, 'make.py', '-compiler', 'ios', '-config', config, '-build', '-vector_mix_test', '-clean', '-nosimd']
 			cmd_args.append([x for x in args if x])
 
 	if platform.system() == 'Darwin' or platform.system() == 'Linux':
 		# Emscripten
-		args = [python_exe, 'make.py', '-compiler', 'emscripten', '-config', 'debug', '-build', '-clean']
+		args = [python_exe, 'make.py', '-compiler', 'emscripten', '-config', 'debug', '-build', '-vector_mix_test', '-clean']
 		cmd_args.append([x for x in args if x])
-		args = [python_exe, 'make.py', '-compiler', 'emscripten', '-config', 'release', '-build', '-unit_test', '-clean']
+		args = [python_exe, 'make.py', '-compiler', 'emscripten', '-config', 'release', '-build', '-unit_test', '-vector_mix_test', '-clean']
 		cmd_args.append([x for x in args if x])
 
 	root_dir = os.path.join(os.getcwd(), '../..')
