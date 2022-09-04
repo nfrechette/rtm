7fe41bbedbe17ff692d50e987ecd81fcb6a3075f
Nicholas Frechette <zeno490@gmail.com>
2022-09-04 11:12:10 -0400

    feat(bench): add new mask_all_equal NEON variant

--- tools/bench/sources/bench_mask_all_equal.cpp
+++ tools/bench/sources/bench_mask_all_equal.cpp
@@ -61,19 +61,48 @@ RTM_DISABLE_SECURITY_COOKIE_CHECK RTM_FORCE_NOINLINE bool RTM_SIMD_CALL mask_all
 // With AVX, cmp4f is a bit faster, reason unclear. Assembly remains shorter and xor
 // can execute on more ports than pcmpeqd and reciprocal throughput is faster and
 // it should avoid a domain change.
-// Wins on Apple M1
 RTM_DISABLE_SECURITY_COOKIE_CHECK RTM_FORCE_NOINLINE bool RTM_SIMD_CALL mask_all_equal_xor4f(mask4f_arg0 lhs, mask4f_arg1 rhs) RTM_NO_EXCEPT
 {
 #if defined(RTM_SSE2_INTRINSICS)
 	// Assembly for this is 11 bytes with SSE2 (13 bytes with AVX): xorps, movmskps, test, sete, ret
 	return _mm_movemask_ps(_mm_xor_ps(lhs, rhs)) == 0;
 #elif defined(RTM_NEON_INTRINSICS)
+	// Generates the following assembly:
+	// eor.16b v0, v1, v0
+    // ext.16b v1, v0, v0, #0x8
+    // zip1.8b v2, v0, v1
+    // zip2.8b v0, v0, v1
+    // zip1.4h v0, v2, v0
+    // fmov   w8, s0
+    // cmp    w8, #0x0
+    // cset   w0, eq
+    // ret
+
 	uint8x16_t mask = vreinterpretq_u8_u32(veorq_u32(vreinterpretq_u32_f32(lhs), vreinterpretq_u32_f32(rhs)));
 	uint8x8x2_t mask_0_8_1_9_2_10_3_11_4_12_5_13_6_14_7_15 = vzip_u8(vget_low_u8(mask), vget_high_u8(mask));
 	uint16x4x2_t mask_0_8_4_12_1_9_5_13_2_10_6_14_3_11_7_15 = vzip_u16(vreinterpret_u16_u8(mask_0_8_1_9_2_10_3_11_4_12_5_13_6_14_7_15.val[0]), vreinterpret_u16_u8(mask_0_8_1_9_2_10_3_11_4_12_5_13_6_14_7_15.val[1]));
 	return vget_lane_u32(vreinterpret_u32_u16(mask_0_8_4_12_1_9_5_13_2_10_6_14_3_11_7_15.val[0]), 0) == 0;
 #endif
 }
+
+#if defined(RTM_NEON_INTRINSICS)
+// On an Apple M1, both mask_all_equal_xor4f and this variant have the same performance
+// but this version has much short assembly
+RTM_DISABLE_SECURITY_COOKIE_CHECK RTM_FORCE_NOINLINE bool RTM_SIMD_CALL mask_all_equal_xor_movn(mask4f_arg0 lhs, mask4f_arg1 rhs) RTM_NO_EXCEPT
+{
+	// Generates the following assembly:
+	// eor.16b v0, v1, v0
+	// xtn.4h v0, v0
+	// fmov   x8, d0
+	// cmp    x8, #0x0
+	// cset   w0, eq
+	// ret
+
+	uint32x4_t mask = veorq_u32(vreinterpretq_u32_f32(lhs), vreinterpretq_u32_f32(rhs));
+	uint16x4_t truncated_mask = vmovn_u32(mask);
+	return vget_lane_u64(vreinterpret_u64_u16(truncated_mask), 0) == 0;
+}
+#endif
 #endif
 
 static void bm_mask_all_equal_scalar4f(benchmark::State& state)
@@ -188,4 +217,34 @@ static void bm_mask_all_equal_xor4f(benchmark::State& state)
 }
 
 BENCHMARK(bm_mask_all_equal_xor4f);
+
+#if defined(RTM_NEON_INTRINSICS)
+static void bm_mask_all_equal_xor_movn(benchmark::State& state)
+{
+	mask4f m0 = mask_set(true, false, true, false);
+	mask4f m1 = mask_set(true, true, false, false);
+	mask4f m2 = mask_set(false, true, false, true);
+	mask4f m3 = mask_set(false, false, false, false);
+
+	volatile bool r0 = false;
+	volatile bool r1 = false;
+	volatile bool r2 = false;
+	volatile bool r3 = false;
+
+	for (auto _ : state)
+	{
+		r0 ^= mask_all_equal_xor_movn(m0, m1);
+		r1 ^= mask_all_equal_xor_movn(m1, m2);
+		r2 ^= mask_all_equal_xor_movn(m2, m3);
+		r3 ^= mask_all_equal_xor_movn(m3, m0);
+	}
+
+	benchmark::DoNotOptimize(r0);
+	benchmark::DoNotOptimize(r1);
+	benchmark::DoNotOptimize(r2);
+	benchmark::DoNotOptimize(r3);
+}
+
+BENCHMARK(bm_mask_all_equal_xor_movn);
+#endif
 #endif
