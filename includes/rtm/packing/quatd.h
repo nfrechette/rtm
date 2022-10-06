da81ac2b8a8e117291612be9e614d8416d362834
Nicholas Frechette <zeno490@gmail.com>
2022-10-05 22:08:37 -0400

    docs(quat): clarify that the result of quat_from_positive_w might not be quite normalized

--- includes/rtm/packing/quatd.h
+++ includes/rtm/packing/quatd.h
@@ -48,6 +48,10 @@ namespace rtm
 	//////////////////////////////////////////////////////////////////////////
 	// Returns a quaternion constructed from a vector3 representing the [xyz]
 	// components while reconstructing the [w] component by assuming it is positive.
+	// Note: When the squared length of [xyz] is very small, the square-root might not
+	// be very accurate when [w] is reconstructed. As such, the resulting quaternion
+	// might be near but not quite normalized. If high accuracy is required, make
+	// sure to normalize explicitly afterwards.
 	//////////////////////////////////////////////////////////////////////////
 	RTM_DISABLE_SECURITY_COOKIE_CHECK RTM_FORCE_INLINE quatd quat_from_positive_w(const vector4d& input) RTM_NO_EXCEPT
 	{
