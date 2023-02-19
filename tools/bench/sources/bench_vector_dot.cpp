8c44b0e4d3b82b1c7e36417a4855d247485570ed
Nicholas Frechette <zeno490@gmail.com>
2023-02-18 19:29:07 -0500

    feat(bench): split neon and neon64 vector dot variants

--- tools/bench/sources/bench_vector_dot.cpp
+++ tools/bench/sources/bench_vector_dot.cpp
@@ -46,6 +46,27 @@ RTM_FORCE_NOINLINE float RTM_SIMD_CALL vector_dot_scalar(vector4f_arg0 lhs, vect
 #if defined(RTM_NEON_INTRINSICS)
 RTM_FORCE_NOINLINE float RTM_SIMD_CALL vector_dot_neon(vector4f_arg0 lhs, vector4f_arg1 rhs) RTM_NO_EXCEPT
 {
+	// Compiles down to this with ARM64:
+	// fmul.4s v0, v0, v1
+    // ext.16b v1, v0, v0, #0x8
+    // fadd.2s v0, v0, v1
+    // faddp.2s v0, v0, v0
+	float32x4_t x2_y2_z2_w2 = vmulq_f32(lhs, rhs);
+	float32x2_t x2_y2 = vget_low_f32(x2_y2_z2_w2);
+	float32x2_t z2_w2 = vget_high_f32(x2_y2_z2_w2);
+	float32x2_t x2z2_y2w2 = vadd_f32(x2_y2, z2_w2);
+	float32x2_t x2y2z2w2 = vpadd_f32(x2z2_y2w2, x2z2_y2w2);
+	return vget_lane_f32(x2y2z2w2, 0);
+}
+#endif
+
+#if defined(RTM_NEON64_INTRINSICS)
+RTM_FORCE_NOINLINE float RTM_SIMD_CALL vector_dot_neon64(vector4f_arg0 lhs, vector4f_arg1 rhs) RTM_NO_EXCEPT
+{
+	// Compiles down to:
+	// fmul.4s v0, v0, v1
+    // faddp.4s v0, v0, v0
+    // faddp.2s s0, v0
 	float32x4_t x2_y2_z2_w2 = vmulq_f32(lhs, rhs);
 	return vaddvq_f32(x2_y2_z2_w2);
 }
@@ -126,6 +147,36 @@ static void bm_vector_dot_neon(benchmark::State& state)
 }
 
 BENCHMARK(bm_vector_dot_neon);
+#endif
+
+#if defined(RTM_NEON_INTRINSICS)
+static void bm_vector_dot_neon64(benchmark::State& state)
+{
+	vector4f v0 = vector_set(-1.0F, 1.0F, -2.0F, -123.134F);
+	vector4f v1 = vector_set(-1.021F, 1.120F, -2.0331F, -1231.134F);
+	vector4f v2 = vector_set(-11.0F, 1.330F, -21.50F, -1253.134F);
+	vector4f v3 = vector_set(0.15F, 115.60F, 21.0221F, 123.13134F);
+
+	float sum0 = 0.0F;
+	float sum1 = 0.0F;
+	float sum2 = 0.0F;
+	float sum3 = 0.0F;
+
+	for (auto _ : state)
+	{
+		sum0 += vector_dot_neon64(v0, v1);
+		sum1 += vector_dot_neon64(v1, v2);
+		sum2 += vector_dot_neon64(v2, v3);
+		sum3 += vector_dot_neon64(v3, v0);
+	}
+
+	benchmark::DoNotOptimize(sum0);
+	benchmark::DoNotOptimize(sum1);
+	benchmark::DoNotOptimize(sum2);
+	benchmark::DoNotOptimize(sum3);
+}
+
+BENCHMARK(bm_vector_dot_neon64);
 #endif
 
 #if defined(RTM_SSE2_INTRINSICS)
