2751a4f8d1f21b44942a756b9a6e95dd6ebb8291
Nicholas Frechette <zeno490@gmail.com>
2023-09-18 22:40:47 -0400

    fix: explicitly add/delete constructors and assignment operators

--- includes/rtm/impl/error.h
+++ includes/rtm/impl/error.h
@@ -115,6 +115,8 @@ RTM_IMPL_FILE_PRAGMA_PUSH
 
 		class runtime_assert final : public std::runtime_error
 		{
+			runtime_assert() = delete;					// Default constructor not needed
+
 			using std::runtime_error::runtime_error;	// Inherit constructors
 		};
 
